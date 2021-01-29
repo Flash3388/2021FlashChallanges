921b7fe15398c00d92dee6f6ba26d51966d5aed5
Tom Tzook <tomtzook@gmail.com>
2021-01-30 01:11:42 +0200

    removed some files

--- challenges/ch5/solution/leaderbot/src/main/java/bot/BotConstants.java
+++ challenges/ch5/solution/leaderbot/src/main/java/bot/BotConstants.java
@@ -2,6 +2,6 @@ package bot;
 
 public class BotConstants {
 
-    public static final String TOKEN = "ODA0MzMyMjI1MjEzNzU5NTQ4.YBKywg.F5u2eEO67hfE1cAReH36ldHEdaY";
+    public static final String TOKEN = "";
     public static final String CHANNEL_NAME = "channel_name";
 }
