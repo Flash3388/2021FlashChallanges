b8a740732dced27c0597121563d581d62672971c
Tom Tzook <tomtzook@gmail.com>
2021-02-24 13:28:43 +0200

    Update Main.java

--- challenges/ch7/solution/src/main/java/app/Main.java
+++ challenges/ch7/solution/src/main/java/app/Main.java
@@ -9,11 +9,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         File pdfFile = new File("/home/tomtzook/Documents/Books/war-and-peace.pdf");
-        String text = "sometime";
+        int[][] encoded = new int[][] {
+                {2,6}, {14,175}, {8,142}, {4,21}, {49,266}, {2,6},
+                {663,269}, {4,2}, {4,50}, {65,166}, {5,108},
+                {5,89}, {5,227}, {2,6}, {663,269}, {2,21}, {21,58}
+        };
         try (Pdf pdf = new Pdf(pdfFile)) {
             Cipher cipher = new Cipher(pdf);
-            int[][] en = cipher.encode(text);
-            System.out.println(cipher.decode(en));
+            System.out.println(cipher.decode(encoded));
         }
     }
 }
